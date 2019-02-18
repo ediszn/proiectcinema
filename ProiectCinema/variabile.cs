9146b7a0774abea34535cc7516aa875bcce8b958
ediszn <szeneduard@gmail.com>
2019-02-18 23:18:10 +0200

    no more movie variables

--- ProiectCinema/variabile.cs
+++ ProiectCinema/variabile.cs
@@ -9,13 +9,5 @@ namespace ProiectCinema
     class variabile
     {
         public static string serverName = "szenql" ;  // change this according to your SQL server
-        public static string film1 = "Aquaman";
-        public static string film2 = "The Grinch";
-        public static string film3 = "Creed 2";
-        public static string film4 = "The Boy Next Door";
-        public static string film5 = "The Dungeon Master";
-        public static string film6 = "Venom";
-        public static string film7 = "Your Mother And I";
-        public static string film8 = "The Neighbors";
     }
 }
